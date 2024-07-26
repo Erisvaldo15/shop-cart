547f8b3a20eae8ccbc3d0b574764f2732e78528e
Erisvaldo15 <erisvaldosilvadesousa38@gmail.com>
2024-07-26 20:29:53 -0300

    Docker added to project

--- src/app/database/Connection.php
+++ src/app/database/Connection.php
@@ -9,7 +9,7 @@ class Connection
 
     public static function connection()
     {
-        return new PDO("mysql:host={$_ENV["DATABASE_HOST"]}; dbname={$_ENV["DATABASE_NAME"]}; 
+        return new PDO("mysql:host={$_ENV["DATABASE_HOST"]}; port={$_ENV["DATABASE_PORT"]}; dbname={$_ENV["DATABASE_NAME"]}; 
         charset=utf8mb4", $_ENV["DATABASE_USER"], $_ENV["DATABASE_PASSWORD"]);
     }
 }
